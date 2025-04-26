35db7bd6f08f7c03b70e261bf5a1263ef9e07608
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-04-26 19:56:30 +0530

    hgj

--- LittleLemonApp/LittleLemonApp/Screens/Components/AppCheckBoxView.swift
+++ LittleLemonApp/LittleLemonApp/Screens/Components/AppCheckBoxView.swift
@@ -9,15 +9,12 @@ import SwiftUI
 
 struct iOSCheckboxToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
-        // 1
         Button(action: {
 
-            // 2
             configuration.isOn.toggle()
 
         }, label: {
             HStack {
-                // 3
                 Image(systemName: configuration.isOn ? "checkmark.square.fill" : "square")
 
                 configuration.label
