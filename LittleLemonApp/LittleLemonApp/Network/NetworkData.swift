cecbb924dd6406ec5ebed82f596e33cab25ba2e6
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-04-26 19:01:35 +0530

    gf

--- LittleLemonApp/LittleLemonApp/Network/NetworkData.swift
+++ LittleLemonApp/LittleLemonApp/Network/NetworkData.swift
@@ -8,22 +8,6 @@
 import CoreData
 import SwiftUI
 
-//class DishesData:ObservableObject{
-//    @Published var menuItems = [MenuItem]()
-//
-//    func reload() async {
-//        let url = URL(string: kURL)!
-//
-//        let urlSession = URLSession.shared
-//          do {
-//            let (data, _) = try await urlSession.data(from: url)
-//            let fullMenu = try JSONDecoder().decode(MenuList.self, from: data)
-//            menuItems = fullMenu.menu
-//         }
-//         catch { }
-//      }
-//}
-
 struct Menu {
     
     func getMenuData(_ context:NSManagedObjectContext){
